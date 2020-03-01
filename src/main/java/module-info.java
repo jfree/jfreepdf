9fc21e8b8e18e17d4d91ab7e6eafbe6e951e0324
David Gilbert <dave@jfree.org>
2020-03-01 05:55:05 +0100

    Add link to GitHub page.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 /**
  * JFreePDF provides an implementation of the {@code Graphics2D} API that 
  * produces output in Adobe's Portable Document Format (PDF).
+ * <p>For the latest information about JFreePDF, please refer to the project page
+ * at GitHub: <a href="https://github.com/jfree/jfreepdf">https://github.com/jfree/jfreepdf</a>
  */
 module org.jfree.pdf {
     requires java.base;
